dc7ed1ccf63abc13b5cf0ac6de9abb01d1f25dfc
kirtigopalcric <118678673+kirtigopalcric@users.noreply.github.com>
2023-03-26 21:59:16 +0530

    Delete artist.routes.js

--- artist.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const artistController = require('../controllers/artist.controller');
-
-//find all artists
-router.get('/artists', artistController.findAllArtists);
-
-module.exports = router;
